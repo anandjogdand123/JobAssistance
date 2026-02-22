c1656465a5e926ecd76a1cfec6a85d1b5bf716d6
ANAND_JOGDAND <86175289+anandjogdand123@users.noreply.github.com>
2026-02-22 08:09:38 +0530

    Add files via upload

---
+++ JobAssistant.Presentation/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using JobAssistant.Application.UseCases;
+using JobAssistant.Domain.Interfaces;
+using JobAssistant.Infrastructure.Scrapers;
+using JobAssistant.Infrastructure.Matchers;
+using JobAssistant.Infrastructure.Appliers;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddScoped<IJobScraper, FakeScraper>();
+        services.AddScoped<IJobMatcher, SimpleMatcher>();
+        services.AddScoped<IJobApplier, ConsoleApplier>();
+
+        services.AddScoped<ApplyJobsUseCase>();
+    })
+    .Build();
+
+var useCase = host.Services.GetRequiredService<ApplyJobsUseCase>();
+
+string resume = ".NET C# Azure SQL";
+
+await useCase.ExecuteAsync(".NET Developer", resume);
+
+Console.WriteLine("DONE");
